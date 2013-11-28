fcaeab4b8d4b54b484d8f7c8d517c08c12a77c96
Ilya Batiy <ilya@ostrovok.ru>
2013-11-29 03:49:51 +0400

    remove LOOKUP_SEP

--- tastypie/resources.py
+++ tastypie/resources.py
@@ -6,6 +6,7 @@ from django.conf.urls.defaults import patterns, url
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned, ValidationError
 from django.core.urlresolvers import NoReverseMatch, reverse, resolve, Resolver404, get_script_prefix
 from django.db import transaction
+from django.db.models.constants import LOOKUP_SEP
 from django.db.models.sql.constants import QUERY_TERMS
 from django.http import HttpResponse, HttpResponseNotFound
 from django.utils.cache import patch_cache_control
